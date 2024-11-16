53c6c426c1fe2ca4da3c5d995091e37527ab89ee
taishin-miyamoto <taishin.miyamoto@dena.com>
2024-11-16 17:49:56 +0900

    プロジェクトを追加

---
+++ NeighborHarada/NeighborHaradaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeighborHaradaApp.swift
+//  NeighborHarada
+//
+//  Created by Taishin Miyamoto on 2024/11/16.
+//
+
+import SwiftUI
+
+@main
+struct NeighborHaradaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
